23a8cd648b93141dc38b5a81988f440478f3d130
Nikita Liakhovka <n.liakhovka@gmail.com>
2024-02-18 20:44:21 +0900

    add main color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        main: colors.blue[600],
+      },
+    },
+  },
+  plugins: [],
+};
